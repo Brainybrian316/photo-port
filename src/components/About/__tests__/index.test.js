b81b4cc083ac4e9692e235c3d9869396b84e2b2e
Brainybrian316 <brainybrian316@gmail.com>
2022-06-26 16:57:16 -0400

    about test

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -9,4 +9,9 @@ describe('About component', () => {
   it('renders',  () => {
     render(<About />);
   });
+
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  })
 });
